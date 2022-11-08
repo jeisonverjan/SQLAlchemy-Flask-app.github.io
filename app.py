372907fdc45a7342025193580aafc11396e1a36b
Verjan <jeisonverjan@gmail.com>
2022-11-07 19:46:31 -0500

    adding endpoints

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from routes.contacts import contacts
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.secret_key = 'SecretKey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin123*@localhost/contactsdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
--- routes/contacts.py
+++ routes/contacts.py
@@ -1,36 +1,58 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from models.contact import Contact
 from utils.db import db
-import json
 
 contacts = Blueprint("contacts", __name__)
 
 
 @contacts.route("/")
 def home():
-    return render_template("index.html")
+    contacts = Contact.query.all()
+    return render_template("index.html", contacts=contacts)
 
 
-@contacts.route("/new", methods=['POST'])
+@contacts.route("/new", methods=["POST"])
 def add_contact():
-    
-    full_name = request.form['full_name']
-    email = request.form['email']
-    phone = request.form['phone']
-    
+
+    full_name = request.form["full_name"]
+    email = request.form["email"]
+    phone = request.form["phone"]
+
     new_contact = Contact(fullname=full_name, email=email, phone=phone)
-    
+
     db.session.add(new_contact)
     db.session.commit()
     
-    return redirect('/')
+    flash('Contact added successfully')
+
+    return redirect(url_for("contacts.home"))
 
 
-@contacts.route("/update")
-def update():
-    return "Updating contact"
+@contacts.route("/update/<id>", methods=["POST", "GET"])
+def update(id):
+    
+    contact = Contact.query.get(id)
+    
+    if request.method == 'POST':
+        contact.fullname = request.form['full_name']
+        contact.email = request.form['email']
+        contact.phone = request.form['phone']
+
+        db.session.commit()
+        
+        flash('Contact updated successfully')
+        
+        return redirect(url_for("contacts.home"))    
+
+    return render_template("update.html", contact=contact)
 
 
-@contacts.route("/delete")
-def delete():
-    return "Deleting contact"
+@contacts.route("/delete/<id>")
+def delete(id):
+    contact = Contact.query.get(id)
+    db.session.delete(contact)
+    db.session.commit()
+    
+    flash('Contact deleted successfully')
+    
+    return redirect(url_for("contacts.home"))
